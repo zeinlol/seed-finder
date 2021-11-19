d10493c7098b0b0243e5397fe8fa87a5ec4694a2
Николай Борщёв <nickolaiborshchov@gmail.com>
2021-11-19 03:01:40 +0200

    idle

--- core/middleware/check_input/main_checker.py
+++ core/middleware/check_input/main_checker.py
@@ -1,7 +1,5 @@
 from arguments import arguments
-from config.main_classes import scan_config
 from core.middleware.check_input.cpu_analyze import check_cpu
-from core.middleware.check_input.seeds import get_seeds_from_cli
 
 
 def check_input() -> None:
