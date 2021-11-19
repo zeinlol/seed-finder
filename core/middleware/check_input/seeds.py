3cd27e67a42ae9aba64423097dd83b5b62007022
Николай Борщёв <nickolaiborshchov@gmail.com>
2021-11-19 03:01:16 +0200

    fix seeds input

--- core/middleware/check_input/seeds.py
+++ core/middleware/check_input/seeds.py
@@ -4,6 +4,7 @@ from arguments import arguments
 def get_seeds_from_cli() -> list[str]:
     seeds = []
     for seed in arguments.seed:
+        seed = seed.replace('!', '-')
         if seed[-1:] == ',':
             seed = seed[:-1]
         seeds.append(seed)
